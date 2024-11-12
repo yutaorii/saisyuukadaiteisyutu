f3cb089db5e647cbc3d443a409b87369216bdfc1
yutaorii <oril.gonzales@icloud.com>
2024-11-12 18:12:31 +0900

    修正致しました。よろしくお願いいたします。

--- src/main/java/com/techacademy/entity/Report.java
+++ src/main/java/com/techacademy/entity/Report.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -16,9 +18,9 @@ import lombok.Data;
 @Table(name = "reports")
 public class Report {
 
-    // ID
+ // ID
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     // 日付
